9b82a9e0efc787b836c65fab940355805f01d70a
Philip Johnson <philip@uptechstudio.com>
2024-03-20 13:14:56 -0700

    Add ApiClient with `track` function
    This adds a new class, an ApiClient, that has the same interface as the
    client in Flutter (minus the batch tracker, as that will come later).
    
    It takes in a single event and posts it to the server. The track
    function returns a boolean on whether it was successful or not.

---
+++ src/http-utils.ts
@@ -0,0 +1,13 @@
+async function authenticatedPost(url: string, apiKey: string, data = {}): Promise<Response> {
+  const headers = {
+    "Content-Type": "application/json",
+    "authorization": `Basic ${apiKey}`
+  }
+
+  return fetch(url, {
+    method: "POST",
+    headers,
+    mode: 'cors',
+    body: JSON.stringify(data),
+  })
+}
