975427572f37eaecd1092d2530afd94888154306
Philip Johnson <philip@uptechstudio.com>
2024-03-20 16:22:06 -0700

    Rename metric event
    This updates the name of MetricEvent so that the purpose is more clear.

--- src/models/metric-event-dto.ts
+++ src/models/metric-event-dto.ts
@@ -1,4 +1,13 @@
+interface MetricEventDtoPayload {
+  readonly eventId: string;
+  readonly name: string;
+  readonly userId?: string;
+  readonly anonymousId?: string;
+  readonly properties?: Record<string, string>;
+  readonly systemProperties?: Record<string, string>;
+}
+
 interface MetricEventDto {
   readonly occurredAt: string;
-  readonly payload: MetricEvent;
+  readonly payload: MetricEventDtoPayload;
 }
--- src/models/metric-event.ts
+++
@@ -1,8 +0,0 @@
-interface MetricEvent {
-  readonly eventId: string;
-  readonly name: string;
-  readonly userId?: string;
-  readonly anonymousId?: string;
-  readonly properties?: Record<string, string>;
-  readonly systemProperties?: Record<string, string>;
-}
