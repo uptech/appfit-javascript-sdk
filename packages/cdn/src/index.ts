1a152f3afad57e1093dbc9ebbd04c1db17f6c611
Philip Johnson <philip@uptechstudio.com>
2024-05-02 15:32:48 -0700

    More robust error handling for cdn appfit initialization
    This just adds a few more error checks to account for more edge cases
    when the cdn script is loaded.
    
    <!-- ps-id: e3d4005d-e9c3-4b6b-a43e-d3ff807a989a -->

--- packages/cdn/src/index.ts
+++ packages/cdn/src/index.ts
@@ -7,7 +7,7 @@ interface GlobalAppFit {
   origin?: string;
   cache?: {
     events?: { eventName: string; payload: Record<string, string> }[];
-    identity?: string;
+    identity?: unknown;
   };
   trackEvent(eventName: string, payload: Record<string, string>): Promise<void>;
   identifyUser(userId?: string): void;
@@ -25,16 +25,36 @@ declare global {
     return;
   }
 
+  if (!window.AppFit) {
+    console.warn('See documentation for proper AppFit installation');
+    window.AppFit = {
+      apiKey,
+      trackEvent: async () => {},
+      identifyUser: () => {},
+    };
+  }
+
+  if (window.AppFit.client) {
+    console.warn('AppFit client was already installed');
+    return;
+  }
+
   const config = new AppFitBrowserConfiguration(apiKey);
   const client = new AppFit(config, window.AppFit.origin);
   window.AppFit.client = client;
   window.AppFit.trackEvent = client.trackEvent.bind(client);
   window.AppFit.identifyUser = client.identifyUser.bind(client);
 
-  if (window.AppFit.cache?.identity) {
+  if (
+    window.AppFit.cache?.identity &&
+    typeof window.AppFit.cache.identity === 'string'
+  ) {
     window.AppFit.identifyUser(window.AppFit.cache.identity);
   }
-  if (window.AppFit.cache?.events) {
+  if (
+    window.AppFit.cache?.events &&
+    Array.isArray(window.AppFit.cache.events)
+  ) {
     window.AppFit.cache.events.forEach((event) => {
       client.trackEvent(event.eventName, event.payload);
     });
